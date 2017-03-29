ee5d2132ee0f01a5ed2f0345fd71723eb8bbf840
Matt Jeanes <mattjeanes23@gmail.com>
2017-03-30 00:11:44 +0100

    added some logic and animations and stuff
    really just messing around here, but it's cool stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,29 +1,65 @@
 import { Component } from "@angular/core";
+import {EventData} from "data/observable";
+import { Page } from "ui/page";
+import {Color} from "color";
+import {View} from "ui/core/view";
+
+var view: View;
+
+export function pageLoaded(args: EventData) {
+    var page = <Page>args.object;
+    view = page.getViewById<View>("view");
+}
+
 
 @Component({
     selector: "my-app",
     template: `
-    <ActionBar title="My Apple" class="action-bar"></ActionBar>
+    <ActionBar title="My App" class="action-bar"></ActionBar>
     <StackLayout>
+    <Label text="Your email address: {{emailAddress}}"></Label>
       <TextField hint="Email Address" keyboardType="email"
-        autocorrect="false" autocapitalizationType="none"></TextField>
+        autocorrect="false" autocapitalizationType="none" [(ngModel)]="emailAddress"></TextField>
       <TextField hint="Password" secure="true"></TextField>
 
-      <Button text="Sign in" (tap)="test()"></Button>
-      <Button text="Sign up for Groceries"></Button>
+      
+      <Button cssClass="btn-login" [text]="signInButtonText" (tap)="login($event)"></Button>
+      <Button cssClass="btn-test" #signInButton [text]="signUpButtonText" (tap)="test($event)"></Button>
     </StackLayout>
   `
 })
 export class AppComponent {
+    public emailAddress: string = "";
+    public signUpButtonText: string = "Create account";
+    public signInButtonText: string = "Log in";
+    public signUpButtonMode: boolean = false;
     constructor() {
-        console.log({
-            type: "Apple",
-            color: "Red"
-        });
-
         console.log("violets are red");
     }
-    test(){
-        alert("hello!");
+    public login(args: EventData){
+        if(!this.emailAddress){
+            alert("Please enter email address!");
+            return;
+        }
+        if(this.signUpButtonMode){
+            alert(`Signed up with email address ${this.emailAddress}`);
+        }else{
+            alert(`Logged in with email adddress ${this.emailAddress}`);
+        }
+    }
+    public test(args: EventData) {
+        let button = args.object as any;
+        let className = button.className.replace("-animated", "").replace("2", "");
+        button.className = className;
+        this.signUpButtonMode = !this.signUpButtonMode;
+        if(this.signUpButtonMode) {
+            button.className = className + "-animated";
+            this.signUpButtonText = "Back";
+            this.signInButtonText = "Sign up";
+        }else{
+             button.className = className + "-animated2";
+            this.signUpButtonText = "Create account";   
+            this.signInButtonText = "Log in";          
+        }
     }
 }
